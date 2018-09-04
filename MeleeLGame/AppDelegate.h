1cf2aec708d5dc8f7a441d66f1bcc4f38b6ef8e2
Jack Stenglein <jackstenglein@utexas.edu>
2018-09-04 17:32:32 -0500

    Initial Commit

---
+++ MeleeLGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeleeLGame
+//
+//  Created by Jack Stenglein on 9/4/18.
+//  Copyright © 2018 JackStenglein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeleeLGame/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MeleeLGame
+//
+//  Created by Jack Stenglein on 9/4/18.
+//  Copyright © 2018 JackStenglein. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
